e995819943e3144caa695c62408adca87d967e33
Renata Brito Costa <93830634+Renatabc@users.noreply.github.com>
2021-12-14 11:18:02 -0300

    Add files via upload

---
+++ Introducao_JavaScript/teste.js
@@ -0,0 +1,24 @@
+var y = 0;
+var x = 1;
+
+var z = 2;
+
+// variável é o valor declarado no código
+
+var preco = 2;
+var deconto = 0.2;
+const preco = 2;
+var total = preco - desconto;
+
+//const não pode ser um valor manipulado
+
+function soma(a, b) {
+    return a + b;
+}
+
+soma(3, 5);
+//parêntese declara atributos
+
+// return para o caso de voltar a manipular o valor
+
+//console.log caso não queira retornar o valor
